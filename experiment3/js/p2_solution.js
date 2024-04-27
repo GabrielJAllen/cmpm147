76cd0d784cf170cf0e80b24e5bc6f690774bdf2a
GabrielJAllen <gjallen@ucsc.edu>
2024-04-26 23:23:46 -0700

    final version

--- experiment3/js/p2_solution.js
+++ experiment3/js/p2_solution.js
@@ -1,6 +1,6 @@
 // project.js - purpose and description here
 // Author: Gabriel Allen
-// Date: 4/25/24
+// Date: 4/26/24
 
 /* exported generateGrid, drawGrid, gridCode, gridCheck */
 /* global placeTile */
@@ -23,8 +23,8 @@ function generateGrid(numCols, numRows) {
     // Choose a random starting point on the top edge
     let startX = Math.floor(Math.random() * numCols);
     
-    // Choose a random width between 1 and 3
-    let width = Math.floor(Math.random() * 3) + 1;
+    // Choose a random width between 2 and 3
+    let width = Math.floor(Math.random() * 2) + 2;
 
     // Overwrite positions from the starting point out to the width with "w"
     for (let i = 0; i < width; i++) {
@@ -123,32 +123,58 @@ function drawGrid(grid) {
 }
 
 function gridCheck(grid, i, j, target) {
-  if( i >= grid.length || j >= grid[0].length ){
-    return false
-  } else {
-    if( grid[i][j] == target ){
-      return true
-    } else {
-      return false
-    }
-  }
+  return (
+    i >= 0 &&
+    i < grid.length &&
+    j >= 0 &&
+    j < grid[i].length &&
+    grid[i][j] === target
+  );
 }
 
 function gridCode(grid, i, j, target) {
   byte = 0;
-  if( gridCheck(grid, i, j-1, target)){
-    byte |= 1
+  if( gridCheck(grid, i, j-1, target)){//left
+    byte += 1
   }
-  if( gridCheck(grid, i+1, j, target)){
-    byte |= 2
+  if( gridCheck(grid, i-1, j, target)){//top
+    byte += 2
   }
-  if( gridCheck(grid, i, j+1, target)){
-    byte |= 4
+  if( gridCheck(grid, i, j+1, target)){//right
+    byte += 4
   }
-  if( gridCheck(grid, i-1, j, target)){
-    byte |= 8
+  if( gridCheck(grid, i+1, j, target)){//bottom
+    byte += 8
   }
   return byte
+}
+
+const lookup = [
+  [18,18],//no nieghbors--SHOULD NOT OCCUR
+  [11,0],//left neighbor only
+  [18,18],//top neighbor only--SHOULD NOT OCCUR
+  [11,1],//left and top neighbor
+  [9,0],//right nieghbor only
+  [18,18],//right and left neighbor--SHOULD NOT OCCUR
+  [9,1],//right and top neighbor
+  [18,18],//right, top, and left neighbor--SHOULD NOT HAVE TILE
+  [18,18],//bottom neighbor only--SHOULD NOT OCCUR
+  [11,0],//bottom and left nieghbor
+  [18,18],//bottom and top neighbor--SHOULD NOT OCCUR
+  [11,1],//bottom, top, and left neighbor
+  [9,0],//bottom and right neighbor
+  [18,18],//bottom, right, and left neighbor--SHOULD NOT OCCUR
+  [9,1],//bottom, right, and top neighbor
+  [18,18],//all neighbor--SHOULD NOT HAVE TILE
+];//look up codes for water
+
+function getAnimatedTileFor(frameCount) {
+  const animationSpeed = 20// How fast the animation cycles (lower is faster)
+  const tileFrames = [[18,18], [18,19], [18,20], [19,20], [20, 20]]
+  
+  // Calculate the current index based on the frame count
+  const index = Math.floor(frameCount / animationSpeed) % tileFrames.length;
+  return tileFrames[index];
 }
 
 function drawContext(grid, i, j, target) {
@@ -161,16 +187,16 @@ function drawContext(grid, i, j, target) {
     // If the current tile is ".", use the corresponding tile index
     placeTile(i, j, (floor(random(4))), 0);
   } else if (grid[i][j] === 'w') {
-    //let code = gridCode(grid, i, j, target);
-    //let tile = lookup[code];
-    //placeTile(i, j, tile[0], tile[1]);
-    placeTile(i, j, 20, 20);
+    const [ti, tj] = getAnimatedTileFor(frameCount);
+    placeTile(i, j, ti, tj);
+    let code = gridCode(grid, i, j, target);
+    if(code != 15 && code != 7 && code != 0 && code != 2 && code != 5 && code != 8 && code != 10 && code != 13){
+      let tile = lookup[code];
+      placeTile(i, j, tile[0], tile[1]);
+    }
   } else {
     // If no matching tile is found, use a default tile
     placeTile(i, j, (floor(random(4))), 0);
   }
 }
 
-const lookup = [
-    
-];
