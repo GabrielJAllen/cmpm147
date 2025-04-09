83e2cd27a6d73b3515e49e66e0c3e6c5e12ea72f
GabrielJAllen <gjallen@ucsc.edu>
2025-04-08 21:37:53 -0700

    final version
    works!

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,34 +1,44 @@
-// project.js - purpose and description here
-// Author: Your Name
-// Date:
+// project.js - Error message generator
+// Author: Gabriel Allen (Vivian)
+// Date: 4/8/25
 
-// NOTE: This is how we might start a basic JavaaScript OOP project
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
+const fillers = {
+  concern: ["Somehow","It would seem","By some grace of god","Hopefully by accident","Through immense incompetence","I don't know how, but","Oh deary","Whoopsie Daisy","How in the **** did"],
+  action: ["deleted", "NULL-ed", "spontatenously created", "incorrectly modified", "bypassed", "overwrote", "completely evaporated", "sent out the airlock", "botched", "FUBAR-ed"],
+  object: ["file", "folder", "pig", "rogue AI", "black hole", "google doc", "new religion", "new computer", "NASA aircraft", "third-world nation's economy", "box of mangoes", "me"],
+  recommendation: ["I'd suggest", "To fix this", "The bible states", "If you were wise", "In my infinite wisdom I'd recommend", "If you'd like to resolve this", "To appease the gods"],
+  course: ["should ctrl-z", "need to contact the CIA", "have to commit a blood sacrifice", "should bath your computer", "need to get outside", "have to find someone more capable", "should step away from the keyboard"]
+};
 
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
-  }
-  
-  // define a method
-  myMethod() {
-    // code to run when method is called
+const template = `Error:
+$concern you $action a $object 
+$recommendation you $course`;
+
+// STUDENTS: You don't need to edit code below this line.
+
+const slotPattern = /\$(\w+)/;
+
+function replacer(match, name) {
+  let options = fillers[name];
+  if (options) {
+    return options[Math.floor(Math.random() * options.length)];
+  } else {
+    return `<UNKNOWN:${name}>`;
   }
 }
 
-function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+function generate() {
+  let story = template;
+  while (story.match(slotPattern)) {
+    story = story.replace(slotPattern, replacer);
+  }
 
-  // call a method on the instance
-  myInstance.myMethod();
+  /* global box */
+  $("#box").text(story);
 }
 
-// let's get this party started - uncomment me
-//main();
+/* global clicker */
+$("#clicker").click(generate);
+
+generate();
